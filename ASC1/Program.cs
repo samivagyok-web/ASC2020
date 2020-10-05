1bf4d3cb036d5f71d81ad07353a6eb78dbe26307
samivagyok-web <66529115+samivagyok-web@users.noreply.github.com>
2020-10-05 16:22:34 +0300

    adaugat cod pentru  P I C I O R

--- ASC1/Program.cs
+++ ASC1/Program.cs
@@ -11,6 +11,22 @@ namespace ASC1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world of c sharp");
+            Console.WriteLine("Introduceti numarul de pantof");
+
+            string line = Console.ReadLine();
+
+            int numarPantof;
+            try
+            {
+                numarPantof = int.Parse(line);
+                Console.WriteLine($"Aveti {numarPantof} numarul la picior");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // int.TryParse(line, out numarPantof);
         }
     }
 }
