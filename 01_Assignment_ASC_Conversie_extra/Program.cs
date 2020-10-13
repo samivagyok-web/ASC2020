75231107fcd55f3bcde047b1b791a900e0a88513
samivagyok-web <samivagyok@gmail.com>
2020-10-13 14:49:13 +0300

    inefficient (i think) way of solving convertion...

--- 01_Assignment_ASC_Conversie_extra/Program.cs
+++ 01_Assignment_ASC_Conversie_extra/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
@@ -11,18 +12,26 @@ namespace _01_Assignment_ASC_Conversie
     {
         static void Main(string[] args)
         {
+
             Console.Write("Introduceti numarul in baza 10 pe care vreti" +
                 " la baza tinta: ");
             int numar = int.Parse(Console.ReadLine());
 
-            Console.Write("Introduceti baza de tinta: ");
-            int bazaTinta = int.Parse(Console.ReadLine());
+            int bazaTinta;
+            do
+            {
+                Console.Write("Introduceti baza de tinta: ");
+                bazaTinta = int.Parse(Console.ReadLine());
+            } while (bazaTinta < 2 || bazaTinta > 16);
+
 
             string result = "";
-            Stack<int> stiva = new Stack<int>();
+
 
             if (bazaTinta >= 2 && bazaTinta <= 9)
             {
+                Stack<int> stiva = new Stack<int>();
+
                 while (numar > 0)
                 {
                     int egesz = numar / bazaTinta;
@@ -30,13 +39,55 @@ namespace _01_Assignment_ASC_Conversie
                     stiva.Push(maradek);
                     numar /= bazaTinta;
                 }
+
+
+                while (stiva.Count > 0)
+                {
+                    result = result + stiva.Pop();
+                }
             }
 
-            while (stiva.Count > 0)
+            if (bazaTinta > 10 && bazaTinta <= 16)
             {
-                result = result + stiva.Pop();
-            }
+                Stack<string> stiva = new Stack<string>();
 
+                while (numar > 0)
+                {
+                    int egesz = numar / bazaTinta;
+                    int maradek = numar % bazaTinta;
+
+                    switch (maradek)
+                    {
+                        case 10:
+                            stiva.Push("A");
+                            break;
+                        case 11:
+                            stiva.Push("B");
+                            break;
+                        case 12:
+                            stiva.Push("C");
+                            break;
+                        case 13:
+                            stiva.Push("D");
+                            break;
+                        case 14:
+                            stiva.Push("E");
+                            break;
+                        case 15:
+                            stiva.Push("F");
+                            break;
+                        default:
+                            stiva.Push($"{maradek}");
+                            break;
+                    }
+                    numar /= bazaTinta;
+                }
+
+                while (stiva.Count > 0)
+                {
+                    result = result + stiva.Pop();
+                }
+            }
             Console.WriteLine(result);
 
         }
