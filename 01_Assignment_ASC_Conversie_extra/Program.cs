049b7fd520c71fe6f960a26981e113d5d25191a3
samivagyok-web <samivagyok@gmail.com>
2020-10-13 12:25:05 +0300

    basic conversion from base10 to base2-9 (no exc check)

--- 01_Assignment_ASC_Conversie_extra/Program.cs
+++ 01_Assignment_ASC_Conversie_extra/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace _01_Assignment_ASC_Conversie
 {
@@ -10,6 +11,33 @@ namespace _01_Assignment_ASC_Conversie
     {
         static void Main(string[] args)
         {
+            Console.Write("Introduceti numarul in baza 10 pe care vreti" +
+                " la baza tinta: ");
+            int numar = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduceti baza de tinta: ");
+            int bazaTinta = int.Parse(Console.ReadLine());
+
+            string result = "";
+            Stack<int> stiva = new Stack<int>();
+
+            if (bazaTinta >= 2 && bazaTinta <= 9)
+            {
+                while (numar > 0)
+                {
+                    int egesz = numar / bazaTinta;
+                    int maradek = numar % bazaTinta;
+                    stiva.Push(maradek);
+                    numar /= bazaTinta;
+                }
+            }
+
+            while (stiva.Count > 0)
+            {
+                result = result + stiva.Pop();
+            }
+
+            Console.WriteLine(result);
 
         }
     }
