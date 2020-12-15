c07eed7e69015f38f84c45ec2d022ee40ffef1ed
samivagyok-web <samivagyok@gmail.com>
2020-12-15 18:57:07 +0200

    asamblor 0.9 version

--- 05_Assingment_ASC_Asamblor/Program.cs
+++ 05_Assingment_ASC_Asamblor/Program.cs
@@ -18,6 +18,7 @@ namespace _05_Assingment_ASC_Asamblor
             string bits = "00000000000000000000000000000000";
 
             int lines = numOfLines();
+            Regex machCode = new Regex(@"\s+\.");
             for (int i = 0; i < lines; i++)
             {
                 StringBuilder bit = new StringBuilder(bits);
@@ -25,84 +26,129 @@ namespace _05_Assingment_ASC_Asamblor
 
                 Regex firstTwo = new Regex(@"(ld|st|addcc|jmpl)");
                 MatchCollection matches = firstTwo.Matches(line);
-
                 if (firstTwo.IsMatch(line))
                 {
                     bit = firstTwoBitsAndOp3(bit, line, firstTwo, matches);
                 }
 
-                Regex numbers = new Regex(@"(%r(\d+)|\[[a-z]\])");
-                MatchCollection numMatches = numbers.Matches(line);
+                Regex reg = new Regex(@"%r\d+");
+                Regex var = new Regex(@"(?<=\[).+?(?=\])");
+                MatchCollection regMatches = reg.Matches(line);
+                MatchCollection varMatches = var.Matches(line);
 
-                registers(bit, line, numbers, numMatches, lines);
-                Console.WriteLine(bit);
+                if (reg.IsMatch(line) && var.IsMatch(line))
+                {
+                    varAndReg(bit, line, reg, regMatches, var, varMatches);
+                }
+
+                if (reg.IsMatch(line) && !(var.IsMatch(line)))
+                {
+                    onlyRegister(bit, line, reg, regMatches);
+                }
+
+                Regex varValue = new Regex(@"(?<=\s+)\d{1,2}$");
+                MatchCollection m = varValue.Matches(line);
+
+                foreach(Match match in m)
+                {
+                    int value = int.Parse(match.ToString());
+                    StringBuilder val = new StringBuilder(conversionToBinary(value).ToString());
+                    string bitVal = val.ToString();
+                    bitVal = bitVal.PadLeft(32, '0');
+                    bit = new StringBuilder(bitVal);
+                }
+
+                if (!machCode.IsMatch(line))
+                {
+                    Console.WriteLine(bit);
+                }
             }
         }
 
-        private static StringBuilder registers(StringBuilder bit, string line, Regex pattern, MatchCollection matches, int lineNum)
+        private static StringBuilder varAndReg(StringBuilder bit, string line, Regex reg, MatchCollection regMatches, Regex var, MatchCollection varMatches)
         {
-            string[] asd = new string[matches.Count];
+            string patHelper = ":";
+            foreach (Match match in varMatches)
+            {
+                string pattern = "^" + match.ToString() + patHelper;
+                Regex varDeclar = new Regex(pattern);
+                int locInMemorie = memorie(varDeclar);
+                bit = regSource2(bit, conversionToBinary(locInMemorie));
+            }
+
+            onlyRegister(bit, line, reg, regMatches);
+
+            return bit;
+        }
+
+        private static StringBuilder onlyRegister(StringBuilder bit, string line, Regex reg, MatchCollection regMatches)
+        {
+            int[] regNumbers = new int[regMatches.Count];
+            Regex digits = new Regex(@"\d{1,2}");
 
             int i = 0;
-            foreach (Match match in matches)
+            foreach(Match match in regMatches)
             {
-                string regNumber = match.ToString();
-                asd[i] = regNumber;
-                i++;
-            }
-            Regex registerNum = new Regex(@"\d{1,2}");
-
-            for (int j = asd.Length - 1; j >= 0; j--)
-            {
-                if (registerNum.IsMatch(asd[j]))
+                string register = match.ToString();
+                MatchCollection matches = digits.Matches(register);
+                
+                foreach (Match match2 in matches)
                 {
-                    MatchCollection number = registerNum.Matches(asd[j]);
-                    foreach (Match match2 in number)
-                    {
-                        int regNum = int.Parse(match2.ToString());
-                        StringBuilder reg = conversionToBinary(regNum);
-
-                        if (regNum == 15)
-                        {
-                            for (int z = 0; z < reg.Length; z++)
-                            {
-                                bit[z + 13] = reg[z];
-                            }
-
-                            StringBuilder four = conversionToBinary(4);
-                            for (int z = 0; z < four.Length; z++)
-                            {
-                                bit[z + 28] = four[z];
-                            }
-                        }
-                        else
-                        {
-                            if (j == asd.Length - 1)
-                            {
-                                for (i = 0; i < reg.Length; i++)
-                                {
-                                    bit[i + 3] = reg[i];
-                                }
-                            }
-
-                            if (j == asd.Length - 2)
-                            {
-                                for (int t = 0; t < reg.Length; t++)
-                                {
-                                    bit[t + 13] = reg[t];
-                                }
-                            }
-
-                            if (j == asd.Length - 3)
-                            {
-                                for (int t = 0; t < reg.Length; t++)
-                                {
-                                    bit[t + 28] = reg[t];
-                                }
-                            }
-                        }
-                    }
+                    regNumbers[i] = int.Parse(match2.ToString());
+                    i++;
                 }
+            }
+
+            if (regNumbers.Length == 3)
+            {
+                bit = regDestination(bit, conversionToBinary(regNumbers[2]));
+                bit = regSource1(bit, conversionToBinary(regNumbers[0]));
+                bit = regSource2(bit, conversionToBinary(regNumbers[1]));
+            }
+            else if (regNumbers.Length == 2)
+            {
+                if (regNumbers[0] == 15)
+                {
+                    bit = regSource2(bit, conversionToBinary(4));
+                    bit = regSource1(bit, conversionToBinary(15));
+                    bit = regDestination(bit, conversionToBinary(regNumbers[1]));
+                }
+                else
+                {
+                    bit = regDestination(bit, conversionToBinary(regNumbers[1]));
+                    bit = regSource1(bit, conversionToBinary(regNumbers[0]));
+                }
+            }
+            else
+            {
+                bit = regDestination(bit, conversionToBinary(regNumbers[0]));
+            }
+            return bit;
+        }
+
+        private static StringBuilder regDestination(StringBuilder bit, StringBuilder rd)
+        {
+            for (int i = 0; i < rd.Length; i++)
+            {
+                bit[i + 2] = rd[i];
+            }
+            return bit;
+        }
+
+        private static StringBuilder regSource1(StringBuilder bit, StringBuilder rs1)
+        {
+            for (int i = 0; i < rs1.Length; i++)
+            {
+                bit[i + 13] = rs1[i];
+            }
+            return bit;
+        }
+
+        private static StringBuilder regSource2(StringBuilder bit, StringBuilder rs2)
+        {
+            for (int i = 0; i < rs2.Length; i++)
+            {
+                bit[i + 27] = rs2[i];
             }
             return bit;
         }
@@ -120,7 +166,7 @@ namespace _05_Assingment_ASC_Asamblor
                 putere = putere * 10;
             }
 
-            string num = numInBin.ToString().PadLeft(4, '0');
+            string num = numInBin.ToString().PadLeft(5, '0');
             StringBuilder numar = new StringBuilder(num);
 
             return numar;
@@ -166,14 +212,14 @@ namespace _05_Assingment_ASC_Asamblor
             return bit;
         }
 
-        static private void memorie(int n)
+        static private int memorie(Regex variable)
         {
             int memo = -4;
             TextReader code = new StreamReader(@"..\..\data.in");
             Regex pattern = new Regex(@"\s+\.");
+            string buffer = " ";
 
-            string buffer = "";
-            for (int i = 0; i < n; i++)
+            while (!variable.IsMatch(buffer))
             {
                 buffer = code.ReadLine();
                 if (!pattern.IsMatch(buffer))
@@ -181,7 +227,7 @@ namespace _05_Assingment_ASC_Asamblor
                     memo = memo + 4;
                 }
             }
-            Console.WriteLine(memo);
+            return memo;
         }
 
         static private int numOfLines()
